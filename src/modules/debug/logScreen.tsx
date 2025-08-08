243a6d751d851b3f38905cd249f9ffceee32e9a2
robmolloy <robmolloy@hotmail.co.uk>
2025-08-08 16:43:03 +0100

    add marketBuyerProfileRecord(s)Store to log screen

--- src/modules/debug/logScreen.tsx
+++ src/modules/debug/logScreen.tsx
@@ -1,15 +1,29 @@
 import { useCurrentUserStore, useUnverifiedIsLoggedInStore } from "@/stores/authDataStore";
 import { useUsersStore } from "../users/usersStore";
+import { useMarketBuyerProfileRecordStore } from "../marketBuyerProfiles/marketBuyerProfileRecordStore";
+import { useMarketBuyerProfileRecordsStore } from "../marketBuyerProfiles/marketBuyerProfileRecordsStore";
 
 export const LogScreen = () => {
   const usersStore = useUsersStore();
   const currentUserStore = useCurrentUserStore();
   const unverifiedIsLoggedInStore = useUnverifiedIsLoggedInStore();
+  const marketBuyerProfileRecordStore = useMarketBuyerProfileRecordStore();
+  const marketBuyerProfileRecordsStore = useMarketBuyerProfileRecordsStore();
 
   return (
     <div>
       <pre>
-        {JSON.stringify({ usersStore, currentUserStore, unverifiedIsLoggedInStore }, undefined, 2)}
+        {JSON.stringify(
+          {
+            usersStore,
+            currentUserStore,
+            unverifiedIsLoggedInStore,
+            marketBuyerProfileRecordStore,
+            marketBuyerProfileRecordsStore,
+          },
+          undefined,
+          2,
+        )}
       </pre>
     </div>
   );
